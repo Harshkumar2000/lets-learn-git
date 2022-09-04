871991f2c9b1f0ffc6c3be03e272ea3a080968f1
Harshkumar <harshkhairnar18@gmail.com>
2022-09-04 12:09:04 +0530

    Firstcode

---
+++ Firstcode.java
@@ -0,0 +1,11 @@
+public class Firstcode{
+
+    public static void main(String[] args){
+
+        int a = 4;
+        int b= 6;
+        int c= a+b;
+        System.out.println("sum of two number ="+c);
+
+    }
+} 
